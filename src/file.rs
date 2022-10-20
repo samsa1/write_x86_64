00c57ce6811b394d2e82a406c96f0e14ee61b114
Samuel VIVIEN <samuel.aime.vivien@free.fr>
2022-10-20 16:21:13 +0200

    removed function that was not in doc

--- src/file.rs
+++ src/file.rs
@@ -1,5 +1,4 @@
 use super::Text;
-use std::collections::HashMap;
 use std::fs;
 use std::io::prelude::*;
 
@@ -12,12 +11,6 @@ pub struct File {
 }
 
 impl File {
-    #[doc(hidden)]
-    pub fn new(text_ss: Text, strings: HashMap<String, String>) -> Self {
-        let data_ss = super::data::Data::from_strings(strings);
-        Self { text_ss, data_ss }
-    }
-
     #[cfg(target_os = "macos")]
     fn write_globl_main(file: &mut fs::File) -> std::io::Result<()> {
         file.write_all(b"\t.globl\t_main\n")
